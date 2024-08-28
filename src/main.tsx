6b302c21613e7b6356163f321172f87af408de15
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-08-28 18:28:38 -0400

    adding background

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./routes/root";
+import App from "./routes/deeper";
+import Welcome from "./routes/root";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Welcome />,
+  },
+  {
+    path: "deeper",
     element: <App />,
   },
 ]);
